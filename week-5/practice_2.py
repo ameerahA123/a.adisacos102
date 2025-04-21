44685b9b9551dadcba028a7ed4fd6209b1fd0a23
ameerahA123 <ameerah.adisa@pau.edu.ng>
2025-04-22 00:13:26 +0100

    (week-5)

--- week-5/practice_2.py
+++ week-5/practice_2.py
@@ -21,7 +21,7 @@ def submit():
     if username == "Mary" and password == "cos102":
         welcomeMessage(username)
     else:
-        messagebox.showerror("Login","Inbalid username or password")
+        messagebox.showerror("Login","Invalid username or password")
 
 root = tk.Tk()
 root.title("Login Form")
@@ -32,4 +32,14 @@ username_label.pack()
 username_entry = tk.Entry(root)
 username_entry.pack()
 
-password_label 
+password_label = tk.Label(root, text="Password:")
+password_label.pack()
+password_entry = tk.Entry(root, show="*")
+password_entry.pack()
+
+# Create submit button
+submit_button = tk.Button(root, text="Submit", command=submit)
+submit_button.pack()
+
+# Run the main event loop
+root.mainloop()
